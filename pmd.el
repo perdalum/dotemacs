746f71725c531c5febba49bd6cedc27be85eb4db
Per Møldrup-Dalum <dalum@me.com>
2017-09-03 13:17:47 +0200

    Just some adjustments

--- pmd.el
+++ pmd.el
@@ -1,19 +1,20 @@
 
+;; Hm, strangely enough, I can't remember why I had to use the below code!?
 ;; syncronise Emacs kill-register (if that is its name) and the macOS clipboard
 ;; code copied from https://emacs.stackexchange.com/questions/10900/copy-text-from-emacs-to-os-x-clipboard
-(defun copy-from-osx ()
-  (shell-command-to-string "pbpaste"))
+;; (defun copy-from-osx ()
+;;   (shell-command-to-string "pbpaste"))
 
-(defun paste-to-osx (text &optional push)
-  (let ((process-connection-type nil))
-    (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
-      (process-send-string proc text)
-      (process-send-eof proc))))
+;; (defun paste-to-osx (text &optional push)
+;;   (let ((process-connection-type nil))
+;;     (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+;;       (process-send-string proc text)
+;;       (process-send-eof proc))))
 
-(setq interprogram-cut-function nil)
-(setq interprogram-paste-function nil)
+;; (setq interprogram-cut-function nil)
+;; (setq interprogram-paste-function nil)
 
-(setq interprogram-cut-function 'paste-to-osx)
-(setq interprogram-paste-function 'copy-from-osx)
+;; (setq interprogram-cut-function 'paste-to-osx)
+;; (setq interprogram-paste-function 'copy-from-osx)
 
 (provide 'pmd)
